cb65a752aa8b341bebafa3eb569ddb8280d4524b
YoungGaLee <43063980+YoungGaLee@users.noreply.github.com>
2022-12-06 09:57:14 +0900

    Update 알람 시계.py

--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -8,3 +8,19 @@ if(M<0):
         H+=24
 
 print(H,M)
+
+
+
+'''
+<모범답안>
+line2를 보면 모든 M
+연산량 관련 공부 이후 마저 피드백
+
+H,M = map(int,input().split())
+if M > 44:
+    print(H, M-45)
+elif M<45 and H>0:
+    print(H-1,M+15)
+else:
+    print(23,M+15)
+'''
