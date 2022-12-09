9ba5f53fbd65df64939dfdf9ab6837c798e6169e
YoungGaLee <43063980+YoungGaLee@users.noreply.github.com>
2022-12-09 14:19:23 +0900

    [Silver V] Title: 셀프 넘버, Time: 48 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,4 +1,4 @@
-omr = [0  for i in range(10000)]
+omr = [0  for i in range(10040)]
 
 def d(const):
     ans = const + sum(list(map(int,str(const))))
@@ -6,10 +6,8 @@ def d(const):
 
 for n in range(10000):
     ans = d(n)
-    if ans > 9999: continue
     omr[ans] = 1
-    
-for index, i in enumerate(omr):
-    if index == 0 : continue
-    if i==0 : print(index)
-    
+
+for i in range(10000):
+    if omr[i] == 0:
+        print(i)
