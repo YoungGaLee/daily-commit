558f42b26ef53a743f9154190a496e249e6ffd0f
YoungGaLee <43063980+YoungGaLee@users.noreply.github.com>
2022-12-09 14:25:27 +0900

    [Silver V] Title: 셀프 넘버, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,4 +1,3 @@
-# 1차
 omr = [0  for i in range(10000)]
 
 def d(const):
@@ -14,29 +13,3 @@ for index, i in enumerate(omr):
     if index == 0 : continue
     if i==0 : print(index)
     
-'''
-크기비교하지말고 
-일단 마킹한다음에 10000번만 출력하면 될거 같음.
-
-그럼 연산량 더 줄까? 
-'''
-
-# 2차
-omr = [0  for i in range(10040)]
-
-def d(const):
-    ans = const + sum(list(map(int,str(const))))
-    return ans
-
-for n in range(10000):
-    ans = d(n)
-    omr[ans] = 1
-
-for i in range(10000):
-    if omr[i] == 0:
-        print(i)
-        
-'''
-결과는 동일 : 30616KB  /  48ms
-그래도 더 깔끔한 듯
-'''
