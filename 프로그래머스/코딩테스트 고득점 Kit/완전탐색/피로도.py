f35621540c72549404ff9a22c6d36d5db93942d7
YoungGaLee <43063980+YoungGaLee@users.noreply.github.com>
2023-08-07 00:22:05 +0900

    Update 피로도.py

--- 프로그래머스/코딩테스트 고득점 Kit/완전탐색/피로도.py
+++ 프로그래머스/코딩테스트 고득점 Kit/완전탐색/피로도.py
@@ -1,35 +1,48 @@
-23.07.27  1차시도 - fail
+# 완전탐색 > 순열
 
-"""
-순서대로X 뭔가 정렬을 해야겠다.
-최소 필요 피로도순? / 소모 피로도순?
-예시가 힌트일까
-예시 : 1-3-2 /// 최소 - 소모 순?
-모든 케이스에서 참이라는 생각은 안들지만 일단 해보자.
-(던전개수도 최대 8이니 ㄱㄱ)
-어우 너무 계산량 많아지는데.. 이렇게 푸는 문제 맞나..
-
-다시보니 저 answer--는 어떤 힌트일까
--1이 되면 멈춰라? 재귀사용?
-"""
-
-
-
-def solution(k, dungeons):
-    diffList = []
-    diffList_ = []
-    for case in dungeons :
-        diff = case[0] - case[1]
-        diffList.append(diff)
-    for order in enumerate(diffList):
-        diffList_.append(order)
-    ans = sorted(diffList_, key=lambda x : x[1])
+from itertools import permutations 
+def recursive(k, case):
     count = 0
-    for num, trash in diffList_:
-        tr, mi = dungeons[num]
-        k-=mi
-        if k <0:
+    for need, spend in case:
+        if need > k:
             break
+        k-=spend
         count+=1
         
     return count
+
+
+def solution(k, dungeons):
+    answer = 0
+    for case in permutations(dungeons):
+        count = recursive(k, case)
+        if answer < count:
+            answer = count
+    return answer
+
+
+'''
+테스트 1 〉	통과 (0.01ms, 10.2MB)
+테스트 2 〉	통과 (0.02ms, 10MB)
+테스트 3 〉	통과 (0.09ms, 10.3MB)
+테스트 4 〉	통과 (0.07ms, 10MB)
+테스트 5 〉	통과 (0.62ms, 10.2MB)
+테스트 6 〉	통과 (2.81ms, 10.2MB)
+테스트 7 〉	통과 (36.39ms, 10.3MB)
+테스트 8 〉	통과 (28.28ms, 10.2MB)
+테스트 9 〉	통과 (0.05ms, 10.2MB)
+테스트 10 〉	통과 (2.63ms, 10.2MB)
+테스트 11 〉	통과 (0.01ms, 10.2MB)
+테스트 12 〉	통과 (17.05ms, 10.2MB)
+테스트 13 〉	통과 (12.62ms, 10.1MB)
+테스트 14 〉	통과 (11.76ms, 10.2MB)
+테스트 15 〉	통과 (11.44ms, 10.3MB)
+테스트 16 〉	통과 (1.41ms, 10.3MB)
+테스트 17 〉	통과 (12.63ms, 10.1MB)
+테스트 18 〉	통과 (0.01ms, 10.3MB)
+테스트 19 〉	통과 (0.05ms, 10.2MB)
+
+채점 결과
+정확성: 100.0
+합계: 100.0 / 100.0
+'''
