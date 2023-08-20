c01db5dffd6cc7a598973b24d1e7b2c5a1f352e5
YoungGaLee <43063980+YoungGaLee@users.noreply.github.com>
2023-08-20 19:50:51 +0900

    Update 의상.py

--- 프로그래머스/코딩테스트 고득점 Kit/해시/의상.py
+++ 프로그래머스/코딩테스트 고득점 Kit/해시/의상.py
@@ -58,3 +58,14 @@ def solution(clothes):
     
     return answer-1
 '''
+
+
+'''
+# 정답예시
+def solution(clothes):
+    from collections import Counter
+    from functools import reduce
+    cnt = Counter([kind for name, kind in clothes])
+    answer = reduce(lambda x, y: x*(y+1), cnt.values(), 1) - 1
+    return answer
+'''
