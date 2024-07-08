c4c09982d0a63775c8546a50ca70b3871da7f507
YoungGaLee <43063980+YoungGaLee@users.noreply.github.com>
2024-07-08 19:45:08 +0900

    Update N으로 표현.py

--- 프로그래머스/코딩테스트 고득점 Kit/동적계획법 DP /N으로 표현.py
+++ 프로그래머스/코딩테스트 고득점 Kit/동적계획법 DP /N으로 표현.py
@@ -1,3 +1,26 @@
+# Tip. set으로 데이터를 저장하면 중복제거할 필요가 없다. 
+# 주의: 리스트명.sort() 메소드는 원본 값을 수정하지만 리턴값은 None이다.
+
+def solution(N, number):
+    arr_num = [set([int(str(N)*(i+1))]) for i in range(8)]
+
+    for j  in range(1,8) :
+        for k in range(j):
+            for n in arr_num[k]:
+                for m in arr_num[j-k-1]:
+                    arr_num[j].add(n + m)
+                    arr_num[j].add(n - m)
+                    arr_num[j].add(n * m)
+                    if m != 0:
+                        arr_num[j].add(n // m)
+
+                if number in arr_num[j]:
+                    pass
+                    return j+1
+
+    return -1
+
+'''
 // testcase 2/9 통과. feedback 필요
 
 import copy
@@ -30,3 +53,4 @@ def solution(N, number):
         in_num = copy.deepcopy(total_num)
 
     return answer
+'''
