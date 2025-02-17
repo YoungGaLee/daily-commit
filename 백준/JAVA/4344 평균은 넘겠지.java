9ac81cfb43230725603562ce2db220d59033dd54
이가영 <43063980+YoungGaLee@users.noreply.github.com>
2025-02-17 12:18:04 +0900

    Update 4344 평균은 넘겠지.java

--- 백준/JAVA/4344 평균은 넘겠지.java
+++ 백준/JAVA/4344 평균은 넘겠지.java
@@ -1,69 +1,3 @@
-import java.util.Scanner;
-import java.util.Arrays;
-
-public class Main{
-    public int inputCaseNum(){
-        Scanner sc = new Scanner(System.in);
-        int caseNum = sc.nextInt();
-        sc.nextLine();
-
-        return caseNum;
-    }
-
-    public int[] inputStudentScore(){
-        Scanner sc = new Scanner(System.in);
-        int studentNum = sc.nextInt();
-        
-        int[] studentScore = new int[studentNum];
-        for (int i=0; i<studentNum; i++){
-            studentScore[i] = sc.nextInt();
-        }
-        return studentScore;
-    }
-
-    public double avg(int[] studentScore){
-        double sum = 0;
-        for (int score : studentScore){
-            sum += score;
-        }
-        double avgScore = sum / studentScore.length;
-        
-        return avgScore;
-    }
-
-   public double checkOverAvg(double avgScore, int[] studentScore){
-        double count = 0;
-        for (int score : studentScore){
-            if (score > avgScore){
-                count += 1;
-            }
-        }
-        return count;
-   }
-
-
-    public void solution(){
-        int caseNum = inputCaseNum();
-
-        for (int i=0; i<caseNum; i++){
-            int[] studentScore = inputStudentScore();
-            double avgScore = avg(studentScore);
-            double aboveStudent = checkOverAvg(avgScore, studentScore);
-            double result = aboveStudent / studentScore.length;
-            System.out.printf("%.3f%%",result*100);
-        }
-    }
-
-    public static void main(String[] args) {
-        Main app = new Main();
-        app.solution();
-    }
-}
-
-
-/*
-
-
 import java.io.*;
 import java.util.*;
 
@@ -78,7 +12,6 @@ public class Main {
 
         for (int i=0; i<N; i++){
             double sum = 0;
-//            int studentNum = Integer.parseInt(String.valueOf(br.read()));
 
             int[] studentScore = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             int studentNum = studentScore[0];
@@ -103,7 +36,7 @@ public class Main {
         br.close();
     }
 }
-*/
+
 
 
 
