3301dfc74d5d7eb1fb1751e946b0c2a7d9ee0528
이가영 <43063980+YoungGaLee@users.noreply.github.com>
2025-02-17 14:58:44 +0900

    Update 2789 유학금지.java

--- 백준/JAVA/2789 유학금지.java
+++ 백준/JAVA/2789 유학금지.java
@@ -1,3 +1,11 @@
+/* 
+아이디어: 입력 문자열에 대해 C > A > M > B > R > I > D > E > G > E 순으로 해당 글자 들어있는지 확인
+    C 확인 후, 입력문자열 업데이트 - 문자열에서 C 사라진 상태
+    A 확인 후, 입력문자열 업데이트 - 문자열에서 A 사라진 상태 
+    ... >> 모두 마친 후, 검열된 값 출력
+*/
+
+
 import java.io.*;
 import java.util.*;
 
@@ -13,14 +21,14 @@ public class Main {
 
         for (int i=0; i<prohibit.length(); i++){
             update = "";
-            for(int j=0; j<inputWord.length(); j++){
-                if (prohibit.charAt(i) == inputWord.charAt(j)){
+            for(int j=0; j<inputWord.length(); j++){ // 업데이트된 문자열의 길이만큼
+                if (prohibit.charAt(i) == inputWord.charAt(j)){ // 해당문자 존재하면 추가하지 않음
                     continue;
-                }else{
+                }else{   // 같지 않은 문자는 업데이트 문자열에 추가
                     update += inputWord.charAt(j);
                 }
             }
-            inputWord = update;
+            inputWord = update; // 업데이트
         }
         result = inputWord;
 
